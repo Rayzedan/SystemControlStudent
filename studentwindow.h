63a35e63b042d199818561eadddaed18806f463c
Daniil <64484372+Rayzedan@users.noreply.github.com>
2022-07-14 15:52:22 +0500

    Update studentwindow.h

--- studentwindow.h
+++ studentwindow.h
@@ -3,11 +3,8 @@
 
 #include <QDialog>
 #include <QWidget>
-<<<<<<< HEAD
 #include "testforstudent.h"
-=======
 #include <QSqlQueryModel>
->>>>>>> 276a248 (добавил парсинг данных из бд в окно студента)
 
 namespace Ui {
 class StudentWindow;
@@ -26,21 +23,13 @@ signals:
 
 private slots:
     void on_pushButton_3_clicked();
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 276a248 (добавил парсинг данных из бд в окно студента)
     void on_pushButton_clicked();
 
 private:
     Ui::StudentWindow *ui;
-<<<<<<< HEAD
     TestForStudent *testWindow;
-=======
     QSqlQueryModel *model;
     QSqlQueryModel *model_dep;
->>>>>>> 276a248 (добавил парсинг данных из бд в окно студента)
 };
 
 #endif // STUDENTWINDOW_H
